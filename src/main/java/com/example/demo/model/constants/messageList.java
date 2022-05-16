011a6006bcac79f51b1399cde9b4efd9f0424d03
david-barrette <davidbar968@gmail.com>
2022-05-15 23:11:55 -0700

    add heroku url to app

--- src/main/java/com/example/demo/model/constants/messageList.java
+++ src/main/java/com/example/demo/model/constants/messageList.java
@@ -2,7 +2,7 @@ package com.example.demo.model.constants;
 
 public class messageList {
     public enum Messages {
-        DEFAULT_MESSAGE("This is a placeholder String!!! Try replacing everything after '.com' in the url with 'com/hello?name=your_name' (replacing 'your_name' with YOUR name silly!)"),
+        DEFAULT_MESSAGE("This is a placeholder String!!! Try replacing everything after '.com' in the url with '/hello?name=your_name' (replacing 'your_name' with YOUR name silly!)"),
 
         ;
 
